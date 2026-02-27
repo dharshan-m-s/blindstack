2a0428656b5ae8cbed88cc4b1c8b409abd56ed9b
dharshan-m-s <dharshanms09@gmail.com>
2026-02-27 11:47:50 +0530

    reset

--- backend/src/main/java/com/skillsphere/repository/SkillRepository.java
+++
@@ -1,8 +0,0 @@
-package com.skillsphere.repository;
-
-import com.skillsphere.entity.Skill;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SkillRepository extends JpaRepository<Skill, Long> {}
